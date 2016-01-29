c8e45c7ddabfa5e2b1e4afe486c25fcf4222bd98
Dai Zhen <daizhenbest@gmail.com>
2016-01-29 17:51:29 +0800

    update code

--- vote.js
+++ vote.js
@@ -1,10 +1,36 @@
 var http = require("http");
 var url = require('url');
+var fs = require("fs");
 var proxyUrl = null;
     //"web-proxy.usa.hp.com";
 var proxyPort = 8080;
-//http://ud.cq.qq.com/api/activity/13451/vote/image/451092/jsonp
-var opt = {  
+
+function LoadProxyList()
+{
+    var proxy_list = [];
+    var data = fs.readFileSync("proxy_list.txt");
+    var dataString = data.toString("utf8");
+    var lineItems = dataString.split('\n');
+    var reg_ip = /(\d+\.\d+\.\d+)\:(\d+)/;
+   // console.log(lineItems.length);
+//    console.log(lineItems[1]);
+    
+    for(var i=0;i<lineItems.length;i++)
+    {
+        var current_item = lineItems[i];
+        if(reg_ip.test(current_item))
+        {
+            var match_result = reg_ip.exec(current_item);
+            //console.log(match_result[1]+":"+match_result[2]);
+            proxy_list.push({"host":match_result[1],"port":match_result[2]});
+        }
+    }
+    
+    return proxy_list;
+}
+
+var opt = 
+{  
         method: "POST",  
         host: "web-proxy.usa.hp.com",
 		port:8080,
@@ -21,83 +47,50 @@ var opt = {
 		}  
     };  
 var proxy_index = 0;
-var proxy_index_2 = 0;
-	var proxy_list=
-		[
-			"web-proxy.china.hp.com",
-			"web-proxy.jpn.hp.com",
-			"web-proxy.atl.hp.com",
-			"web-proxy.sgp.hp.com",
-			"proxy.jpn.hp.com",
-			"proxy.houston.hp.com",
-			"web-proxy.cce.hp.com",
-			"web-proxy.usa.hp.com",
-			"proxy.houston.hpecorp.net"
-		];
-function vote()
+function vote(proxy,url)
 {
-	proxy_index = proxy_index % (proxy_list.length-1);
-	opt.host = proxy_list[proxy_index];
-    opt.path = "http://ud.cq.qq.com/api/activity/13455/vote/image/451098/jsonp";
+	//proxy_index = proxy_index % (proxy_list.length-1);
+	//opt.host = proxy_list[proxy_index];
+    //opt.path = "http://ud.cq.qq.com/api/activity/13455/vote/image/451098/jsonp";
+    opt.host = proxy.host;
+    opt.port = proxy.port;
+    opt.path = url;
 	//console.log(opt.host);
 	
 	//req.write("");
 	try
 	{
 		var req = http.request(opt,function(response)
-		{  var body = "";  
-				/*
-				if (response.statusCode == 200) {  
-					var body = "";  
-					response.on('data', function (data) { body += data; })  
-							.on('end', function () { console.log(body);});  
-				}  
-				else {
-					console.log("Net work error:"+response.statusCode);
-				}  
-				*/
-							response.on('data', function (data) { body += data; })  
-							.on('end', function () { console.log(body);});  
+		{  
+            var body = ""; 
+		    response.on('data', function (data) { body += data; })  
+					.on('end', function () { console.log(body);})
+                    .on('error', function(){console.error("Error to connect to ...")});
 		});
+        req.on("error",function(err){console.error(err)});
 		req.end();
 	}
 	catch(ex)
 	{
-		console.log("Exception:"+ex);
+		console.error("Exception:"+ex);
 	}
-	proxy_index++;
 }
-function vote_2()
+function Process()
 {
-	opt.path="http://ud.cq.qq.com/api/activity/13451/vote/image/451092/jsonp";
-	//console.log(opt.host);
-
-	//req.write("");
-	try
-	{	
-		var req = http.request(opt,function(response)
-		{  var body = "";  
-				/*
-				if (response.statusCode == 200) {  
-					var body = "";  
-					response.on('data', function (data) { body += data; })  
-							.on('end', function () { console.log(body);});  
-				}  
-				else {
-					console.log("Net work error:"+response.statusCode);
-				}  
-				*/
-							response.on('data', function (data) { body += data; })  
-							.on('end', function () { console.log(body);});  
-		});
-		req.end();
-	}
-	catch(ex)
-	{
-		console.log("Exception:"+ex);
-	}
-	proxy_index++;
+    var  proxy_list = LoadProxyList();
+    try
+    {
+        proxy_index = proxy_index % (proxy_list.length-1);
+        
+        console.log(proxy_index+"-----"+proxy_list[proxy_index].host+":"+proxy_list[proxy_index].port);
+        vote(proxy_list[proxy_index],"http://ud.cq.qq.com/api/activity/13455/vote/image/451098/jsonp");
+        vote(proxy_list[proxy_index],"http://ud.cq.qq.com/api/activity/13451/vote/image/451092/jsonp");
+    }
+    catch(ex)
+    {
+           console.error(ex);
+        //nothng to do.
+    }
+    proxy_index++;
 }
-
-setInterval(vote,1000*10);
-setInterval(vote_2,1000*10);
+setInterval(Process,400);
